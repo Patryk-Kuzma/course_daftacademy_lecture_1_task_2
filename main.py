6ff72ce0f4f204b9e2082d20369442d2951284a2
Patryk Kuźma <63072423+Patryk-Kuzma@users.noreply.github.com>
2020-04-04 14:33:02 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,26 +1,19 @@
 from fastapi import FastAPI
-from pydantic import BaseModel
 
 app = FastAPI()
 
-
-class Item(BaseModel):
-    name: str
-    price: float
-    is_offer: bool = None
-
-
 @app.get("/")
 def read_root():
-    return {"Hello": "World"}
+    return {"method": "GET"}
 
+@app.put("/")
+def put():
+    return {"method": "PUT"}
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: str = None):
-    return {"item_id": item_id, "q": q}
+@app.post("/")
+def post():
+    return {"method": "POST"}
 
-
-@app.put("/items/{item_id}")
-def update_item(item_id: int, item: Item):
-    return {"item_name": item.name, "item_id": item_id}
-
+@app.delete("/")
+def delete():
+    return {"method": "DELETE"}
